f03cc5114a6f9beb52ef189b673cc6c21e575d1d
shuaishuai <shuaishuai996@163.com>
2018-11-15 10:32:56 +0800

    提交maven_parent

---
+++ maven_parent/maven_dao/src/main/java/com/shuaishuai/dao/ItemsDao.java
@@ -0,0 +1,7 @@
+package com.shuaishuai.dao;
+
+import com.shuaishuai.domain.Items;
+
+public interface ItemsDao {
+    public Items findById(Integer id);
+}
---
+++ maven_parent/maven_service/src/main/java/com/shuaishuai/service/ItemsService.java
@@ -0,0 +1,8 @@
+package com.shuaishuai.service;
+
+
+import com.shuaishuai.domain.Items;
+
+public interface ItemsService {
+    public Items findById(Integer id);
+}
